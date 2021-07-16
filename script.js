582bf5625450059a4cb453f6182c53df65f8764a
Stephen Kurshner <skurshner@gmail.com>
2021-07-16 15:40:43 -0700

    renamed computerPlay to computerInput, added computerSelection variable, added playerInput function and playerSelection variable

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//Create a function computerPlay that randomly returns "Rock", "Paper", or "Scissors"
-
-function computerPlay() {
+//Create a function computerInput that randomly returns "Rock", "Paper", or "Scissors"
+function computerInput() {
     let number = Math.random();
     if (number <= 0.33) {
         return "Rock";
@@ -11,10 +10,19 @@ function computerPlay() {
     }
 }
 
-let computerSelection = computerPlay();
+//Assign variable computerSelection to output of computerInput
+let computerSelection = computerInput();
 console.log(computerSelection);
 
-//Create a function computerSelection that takes in output of computerPlay and assigns it to a variable
-//Create a function playerSelection that gets the input from the user through a prompt
+//Create a function playerInput that gets the input from the user through a prompt & converts to title case
+function playerInput() {
+    let input = prompt("What is your move?").toLowerCase();
+    return (input[0].toUpperCase()) + input.slice(1);   
+}
+
+//Assign variable playerSelection to output of playerInput
+let playerSelection = playerInput();
+console.log(playerSelection);
+
 //Create a function playRound that takes in computerSelection and playerSelection and determines a winner
 //Create a function game which repeats playRound 5 times
