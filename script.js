6c75fc6cc72dee7564b2df89bada30cc6f999304
Stephen Kurshner <skurshner@gmail.com>
2021-07-17 16:06:36 -0700

    fixed typos and added line breaks to round results.

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function playerInput() {
 let computerScore = 0;
 let playerScore = 0;
 
-//Create a function playRound(x6=5) that takes in computerSelection and playerSelection and determines a winner for each of the 5 games.
+//Create a function playRound(x=5) that takes in computerSelection and playerSelection and determines a winner for each of the 5 games.
 function playRound1() {
     let computerSelection1 = computerInput();
     //console.log(computerSelection1);
@@ -33,10 +33,10 @@ function playRound1() {
             computerSelection1 === "Paper" && playerSelection1 === "Rock" ||
             computerSelection1 === "Scissors" && playerSelection1 === "Paper") {
                 ++computerScore;
-                alert(`You lost, ${ computerSelection1 } beats ${ playerSelection1 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You lost, ${ computerSelection1 } beats ${ playerSelection1 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             } else {
                 ++playerScore;
-                alert(`You won, ${ playerSelection1 } beats ${ computerSelection1 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You won, ${ playerSelection1 } beats ${ computerSelection1 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             }
     }
 }
@@ -53,10 +53,10 @@ function playRound2() {
             computerSelection2 === "Paper" && playerSelection2 === "Rock" ||
             computerSelection2 === "Scissors" && playerSelection2 === "Paper") {
                 ++computerScore;
-                alert(`You lost, ${ computerSelection2 } beats ${ playerSelection2 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You lost, ${ computerSelection2 } beats ${ playerSelection2 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             } else {
                 ++playerScore;
-                alert(`You won, ${ playerSelection2 } beats ${ computerSelection2 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You won, ${ playerSelection2 } beats ${ computerSelection2 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             }
     }
 }
@@ -73,10 +73,10 @@ function playRound3() {
             computerSelection3 === "Paper" && playerSelection3 === "Rock" ||
             computerSelection3 === "Scissors" && playerSelection3 === "Paper") {
                 ++computerScore;
-                alert(`You lost, ${ computerSelection3 } beats ${ playerSelection3 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You lost, ${ computerSelection3 } beats ${ playerSelection3 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             } else {
                 ++playerScore;
-                alert(`You won, ${ playerSelection3 } beats ${ computerSelection3 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You won, ${ playerSelection3 } beats ${ computerSelection3 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             }
     }
 }
@@ -93,10 +93,10 @@ function playRound4() {
             computerSelection4 === "Paper" && playerSelection4 === "Rock" ||
             computerSelection4 === "Scissors" && playerSelection4 === "Paper") {
                 ++computerScore;
-                alert(`You lost, ${ computerSelection4 } beats ${ playerSelection4 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You lost, ${ computerSelection4 } beats ${ playerSelection4 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             } else {
                 ++playerScore;
-                alert(`You won, ${ playerSelection4 } beats ${ computerSelection4 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You won, ${ playerSelection4 } beats ${ computerSelection4 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             }
     }
 }
@@ -113,10 +113,10 @@ function playRound5() {
             computerSelection5 === "Paper" && playerSelection5 === "Rock" ||
             computerSelection5 === "Scissors" && playerSelection5 === "Paper") {
                 ++computerScore;
-                alert(`You lost, ${ computerSelection5 } beats ${ playerSelection5 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You lost, ${ computerSelection5 } beats ${ playerSelection5 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             } else {
                 ++playerScore;
-                alert(`You won, ${ playerSelection5 } beats ${ computerSelection5 }. You: ${ playerScore }, Computer: ${ computerScore }`);
+                alert(`You won, ${ playerSelection5 } beats ${ computerSelection5 }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             }
     }
 }
