0f0c4d45c2831c1a32027a79dcfafc0bad7345f5
Stephen Kurshner <skurshner@gmail.com>
2021-07-21 16:30:44 -0700

    added counters on page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Create a function computerPlay that randomly returns "Rock", "Paper", or "Scissors"
+// Create a function computerPlay that randomly returns "Rock", "Paper", or "Scissors"
 function computerPlay() {
     let number = Math.random();
     if (number <= 0.33) {
@@ -9,53 +9,50 @@ function computerPlay() {
         return "Scissors";
     }
 }
-//Assign and initialize variables for computer and player selection
+// Assign and initialize variables for computer and player selection
 let computerSelection;
 let playerSelection;
 
-
+// Button click event assigns player's move and calls playRound()
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerSelection = (button.id[0].toUpperCase()) + button.id.slice(1); 
-        console.log(playerSelection);
+        //console.log(playerSelection);
         playRound();
     })
 })
 
-/*Create a function playerPlay that gets the input from the user through a prompt & converts to title case
-function playerPlay() {
-    let input = prompt("What is your move ... Rock, Paper or Scissors?").toLowerCase();
-    return (input[0].toUpperCase()) + input.slice(1);     
-}*/
-
-//Assign and initialize variables for counting the score
+// Assign and initialize variables for counting the score
 let computerScore = 0;
+let computerCount = document.querySelector('#computerscore');
 let playerScore = 0;
+let playerCount = document.querySelector('#playerscore')
 
-//Create a function playRound that takes in computerSelection and playerSelection and determines a winner and sets the game score
+// Create a function playRound that takes in computerSelection and playerSelection and determines a winner and sets the game score
 function playRound() {
     computerSelection = computerPlay();
-    console.log(computerSelection);
-    //playerSelection = playerPlay();
+    //console.log(computerSelection);
     
     if (computerSelection === playerSelection) {
-        alert("It's a tie.");
+        //alert("It's a tie.");
     } else {
         if (computerSelection === "Rock" && playerSelection === "Scissors" ||
             computerSelection === "Paper" && playerSelection === "Rock" ||
             computerSelection === "Scissors" && playerSelection === "Paper") {
                 ++computerScore;
-                alert(`You lost this round. ${ computerSelection } beats ${ playerSelection }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
+                computerCount.textContent = computerScore;
+                //alert(`You lost this round. ${ computerSelection } beats ${ playerSelection }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             } else {
                 ++playerScore;
-                alert(`You won this round! ${ playerSelection } beats ${ computerSelection }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
+                playerCount.textContent = playerScore;
+                //alert(`You won this round! ${ playerSelection } beats ${ computerSelection }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
             }
     }
 }
 
-/*Create a function game which plays the five rounds
+/* Create a function game which plays the five rounds
 function game () {
     const roundOne = playRound();
     const roundTwo = playRound();
