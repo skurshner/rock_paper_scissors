e7f111146f0f149d50a643e8cb40d0bd5d441f4e
Stephen Kurshner <skurshner@gmail.com>
2021-07-21 16:45:03 -0700

    added checkWinner() function to detect when either score is 5

--- script.js
+++ script.js
@@ -26,9 +26,21 @@ buttons.forEach((button) => {
 
 // Assign and initialize variables for counting the score
 let computerScore = 0;
-let computerCount = document.querySelector('#computerscore');
 let playerScore = 0;
-let playerCount = document.querySelector('#playerscore')
+
+// Target score counters
+let computerCount = document.querySelector('#computerscore');
+let playerCount = document.querySelector('#playerscore');
+
+// Target outcome
+let outcome = document.querySelector('#outcome');
+
+// When either score reaches 5, hide buttons and announce winner
+function checkWinner() {
+    if (playerScore === 5 || computerScore === 5) {
+        console.log("there is a winner");
+    }
+}
 
 // Create a function playRound that takes in computerSelection and playerSelection and determines a winner and sets the game score
 function playRound() {
@@ -36,36 +48,20 @@ function playRound() {
     //console.log(computerSelection);
     
     if (computerSelection === playerSelection) {
-        //alert("It's a tie.");
+        outcome.textContent = "It's a tie.";
     } else {
         if (computerSelection === "Rock" && playerSelection === "Scissors" ||
             computerSelection === "Paper" && playerSelection === "Rock" ||
             computerSelection === "Scissors" && playerSelection === "Paper") {
                 ++computerScore;
                 computerCount.textContent = computerScore;
-                //alert(`You lost this round. ${ computerSelection } beats ${ playerSelection }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
+                outcome.textContent = `You lost this round. ${ computerSelection } beats ${ playerSelection }`;
             } else {
                 ++playerScore;
                 playerCount.textContent = playerScore;
-                //alert(`You won this round! ${ playerSelection } beats ${ computerSelection }. \r\nYou: ${ playerScore }, Computer: ${ computerScore }`);
+                outcome.textContent = `You won this round! ${ playerSelection } beats ${ computerSelection }`;
             }
     }
+    checkWinner();
 }
 
-/* Create a function game which plays the five rounds
-function game () {
-    const roundOne = playRound();
-    const roundTwo = playRound();
-    const roundThree = playRound();
-    const roundFour = playRound();
-    const roundFive = playRound();
-    
-    if (computerScore === playerScore) {
-        alert("Game ended in a tie. Refresh and try again.")
-    }   else {
-        (computerScore > playerScore) ? alert(`You lost the game ${ computerScore } to ${ playerScore }, refresh to try again!`) : 
-        alert(`You won the game ${ playerScore } to ${ computerScore }, awesome job!`);
-    }
-}
-
-game();*/
